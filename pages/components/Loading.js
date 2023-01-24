2ef28dee026120116d7520a71f038ecae25a2ab6
Amarsanaa <amarsanaa065@gmail.com>
2023-01-24 16:03:33 +0800

    mm

--- pages/components/Loading.js
+++ pages/components/Loading.js
@@ -5,15 +5,15 @@ import loo from '../../public/Frame.png'
 function Loading() {
     return (
         <div className="w-screen h-screen">
-        <div className='flex flex-col justify-center items-center h-[90%]'>
-          <div className=''>
-            <Image src={loo} alt=''/>
-          </div>
+            <div className='flex flex-col justify-center items-center h-[90%]'>
+                <div className=''>
+                    <Image src={loo} alt=''/>
+                </div>
+            </div>
+            <Link href="/main " passHref className='w-screen h-[10%] flex justify-center items-center'>
+                <button className='w-[300px] h-[55px] bg-[#CD5152] flex justify-center items-center rounded-md'>Уншиж байна...</button>
+            </Link>
         </div>
-        <Link href="/main " passHref className='w-screen h-[10%] flex justify-center items-center'>
-          <button className='w-[300px] h-[55px] bg-[#CD5152] flex justify-center items-center rounded-md'>Уншиж байна...</button>
-        </Link>
-       </div>
     );
   }
   
